7c8e54fa2fadd071e15c7b8e719852b4445a7aa9
Nikapwente <hrsmajor@gmail.com>
2023-10-04 07:24:59 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity) {
+
+    if (activity === undefined) {
+        activity = "roller-skate"
+    } else {let activity =  activity}
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity) {
+
+    if (activity === undefined) {
+        activity = "go to the office"
+    } else {let activity =  activity}
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visualFlair="*") {
+    return (function(quality="special") {return `You are ${visualFlair}${quality}${visualFlair}!`})
+}
